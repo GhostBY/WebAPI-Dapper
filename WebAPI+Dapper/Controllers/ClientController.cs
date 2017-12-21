d180c5977b265c7393aebc0b3b0adda566bd7368
Lukashov Alexandr <alexandrlukashov@outlook.com>
2017-12-21 12:54:03 +0300

    complite CRUD operations

---
+++ WebAPI+Dapper/Controllers/ClientController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_Dapper.Models.Entities;
+using WebAPI_Dapper.Models.Interfaces;
+using WebAPI_Dapper.Models.Repository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI_Dapper.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClientController : Controller
+    {
+        private readonly IRepository<Client> _clientRepository = new ClientRepository();
+        [HttpGet]
+        public IEnumerable<Client> Get()
+        {
+            return _clientRepository.GetAll();
+        }
+        // GET: api/values
+       
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Create([FromBody]Client client)
+        {
+            _clientRepository.Create(client);
+            return Ok();
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public void Update([FromBody]Client client)
+        {
+            _clientRepository.Update(client);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Remove(int Id)
+        {
+            _clientRepository.Remove(Id);
+            }
+    }
+}
--- WebAPI+Dapper/Models/Repository/ClientRepository.cs
+++ WebAPI+Dapper/Models/Repository/ClientRepository.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using WebAPI_Dapper.Models.Entities;
 using WebAPI_Dapper.Models.Interfaces;
 
@@ -9,28 +12,41 @@ namespace WebAPI_Dapper.Models.Repository
 {
     public class ClientRepository:IRepository<Client>
     {
-        public ClientRepository()
-        {
-            
-        }
+        private readonly string _connectionString = @"Server=DESKTOP-ARM4FB7\SQLEXPRESS;Database=DapperDB;Initial Catalog=DapperDB;Integrated Security=True";
         public void Create(Client item)
         {
-            throw new NotImplementedException();
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                var sqlQuery = "INSERT INTO Clients (Name, Phone, Mail) VALUES(@Name, @Phone, @Mail);";
+                db.Execute(sqlQuery,item);
+            }
         }
 
         public void Remove(int Id)
         {
-            throw new NotImplementedException();
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                var sqlQuery = "DELETE FROM Clients WHERE Id = @id";
+                db.Execute(sqlQuery, new { Id });
+            }
         }
 
         public void Update(Client item)
         {
-            throw new NotImplementedException();
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                var sqlQuery = "UPDATE Clients SET Name = @Name, Phone = @Phone, Mail = @Mail WHERE Id = @Id";
+                db.Execute(sqlQuery, item);
+            }
         }
 
         public IEnumerable<Client> GetAll()
         {
-            throw new NotImplementedException();
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                IEnumerable<Client> clients= db.Query<Client>("Select * FROM Clients");
+                return clients;
+            }
         }
 
         public Client Get(int Id)
